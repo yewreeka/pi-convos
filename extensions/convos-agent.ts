9e62395081003c05d51a64d02016de6f0d323c95
yewreeka <jarodluebbert@gmail.com>
2026-02-18 13:00:06 -0800

    Remove debug logging

--- extensions/convos-agent.ts
+++ extensions/convos-agent.ts
@@ -173,8 +173,7 @@ export default function (pi: ExtensionAPI) {
           break;
 
         case "message":
-          // Debug: log raw event to see attachment structure
-          console.error(`[convos] raw message event: ${JSON.stringify(event)}`);
+
           lastMessageFromConvos = true;
           pi.sendMessage(
             {
